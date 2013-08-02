52d3649130cd6870ad4a82022f3b03ae6a640278
eherman24 <eherman24@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-02 23:38:03 +0000

    added
    git-svn-id: https://plugins.svn.wordpress.org/svg-vector-icon-plugin/trunk@750350 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/scripts.php
@@ -0,0 +1,11 @@
+<?php
+
+/***********************************
+*	SCRIPT CONTROLS
+***********************************/
+
+function load_icon_set1_style() {
+	wp_register_style( 'svg-icon-set1-style',  plugin_dir_url(__FILE__).'css/wp-svg-icon-set1-plugin-style.css');	wp_enqueue_style( 'svg-icon-set1-style' );
+}
+add_action( 'wp_enqueue_scripts', 'load_icon_set1_style' );
+function load_icon_set1_style_dashboard() {	wp_register_style( 'svg-icon-set1-style-dashboard',  plugin_dir_url(__FILE__).'css/wp-svg-icon-set1-plugin-style.css');	wp_enqueue_style( 'svg-icon-set1-style-dashboard' );}add_action( 'admin_enqueue_scripts', 'load_icon_set1_style_dashboard' );
